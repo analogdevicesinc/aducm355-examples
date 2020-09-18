b62f6d73bbda4c37395571e7e22e0aed103f3e1b
Lambe <Micheal.Lambe@analog.com>
2020-09-18 10:37:04 +0100

    Updated LPTIA switch setting for 3 lead sensor, Set default ADC PGA gain to 1.5

--- examples/ApplicationExamples/M355_SqrWaveVoltammetry/SqrWaveVoltammetry.c
+++ examples/ApplicationExamples/M355_SqrWaveVoltammetry/SqrWaveVoltammetry.c
@@ -1,4 +1,12 @@
-/******************************************************************************
+/*!
+ *****************************************************************************
+ @file:    SWVTest.c
+ @author:  $Author: nxu2 $
+ @brief:   Square Wave Voltammetry measurement sequences.
+ @version: $Revision: 766 $
+ @date:    $Date: 2017-08-21 14:09:35 +0100 (Mon, 21 Aug 2017) $
+ -----------------------------------------------------------------------------
+
 Copyright (c) 2017-2019 Analog Devices, Inc. All Rights Reserved.
 
 This software is proprietary to Analog Devices, Inc. and its licensors.
@@ -7,7 +15,19 @@ Analog Devices Software License Agreement.
 
 *****************************************************************************/
 
-/** 
+/** @addtogroup AD5940_System_Examples
+ * @{
+ *    @defgroup Ramp_Test_Example
+ *    @brief Using sequencer to generate ramp signal and control ADC to sample data.
+ *    @details
+ * @note Need to update code when runs at S2 silicon.
+ * @todo update LPDAC switch settings for S2 and LPDAC 1LSB bug.
+ * @todo Calibrate ADC/PGA firstly to get accurate current. (Voltage/Rtia = Current)
+ * @note The method to calculate LPDAC ouput voltage
+ *        - #define LSB_DAC12BIT (2.2V/4095)
+ *        - #define LSB_DAC6BIT  (2.2V/4095*64)
+ *        - Volt_12bit = Code12Bit*LSB_DAC12BIT + 0.2V
+ *        - Volt_6bit = Code6Bit*LSB_DAC6BIT + 0.2V
  * 
  * # Ramp Signal Parameters definition
  * 
@@ -40,7 +60,7 @@ Analog Devices Software License Agreement.
  * RampState define:    S0 | S1  | S2 |          
  * @endcode
  * 
- *  # The seuqencer method to do Ramp test.
+ *  # The sequencer method to do Ramp test.
  * The Ramp test need to update DAC data in real time to generate required waveform, and control ADC to start sample data. \n
  * We used two kinds of sequence to realize it. One is to control DAC where SEQ0 and SEQ1 are used, another sequence SEQ2 controls ADC. 
  *  ## Sequence Allocation
@@ -85,7 +105,7 @@ Analog Devices Software License Agreement.
  * SEQ2 commands are fixed. Function is simply turn on ADC for a while and turn off it 
  *      after required number of data ready.                                                                \n
  * SEQ0/1 is always changing its start address to update DAC with different voltage.                        \n
- * Check above figure we can see SEQ0/SEQ1 is repeatly trigged by wakuptimer, if we don't change the start
+ * Check above figure we can see SEQ0/SEQ1 is repeatedly trigged by Wakeuptimer, if we don't change the start
  * Address of SEQ0/SEQ1, they will always update DAC with same data, thus no waveform generated.
  * 
  * Considering below SEQ0 command which is similar for SEQ1 on modifying SEQxINFO register.:
@@ -116,10 +136,10 @@ Analog Devices Software License Agreement.
  * @endcode
  * 
  * Total SRAM is 6kB in AD594x. In normal other application, we use 2kB for sequencer and 4kB for FIFO.
- * Assume the ramp test require 128 steps, then the sequence lenght is 4*128 = 512, each command need 4Byte. So it costs 2kB SRAM.
+ * Assume the ramp test require 128 steps, then the sequence length is 4*128 = 512, each command need 4Byte. So it costs 2kB SRAM.
  * When ramp test requires hundres of voltage steps(ADC samples), 2kB SRAM is far from enough. We recommend to use 4kB for sequencer
  * and 2kB for data FIFO. 
- * If ramp test reuire more steps, then we need to update SRAM with commands dynamically, use it as a ping-pong buffer.
+ * If ramp test require more steps, then we need to update SRAM with commands dynamically, use it as a ping-pong buffer.
  * 
  * **Sequencer Command Block 3**
  * @code
@@ -207,11 +227,11 @@ AD5940Err AppSWVGetCfg(void *pCfg)
 
 /**
  * @brief Control application like start, stop.
- * @param Command: The command for this applicaiton, select from below paramters
- *        - APPCTRL_START: start the measurment. Note: the ramp test need firslty call function AppSWVInit() every time before start it.
- *        - APPCTRL_STOPNOW: Stop the measurment immediately.
+ * @param Command: The command for this application, select from below paramters
+ *        - APPCTRL_START: start the measurement. Note: the ramp test need firstly call function AppSWVInit() every time before start it.
+ *        - APPCTRL_STOPNOW: Stop the measurement immediately.
  *        - APPCTRL_STOPSYNC: Stop the measuremnt when current measured data is read back.
- *        - APPCTRL_SHUTDOWN: Stop the measurment immediately and put AFE to shut down mode(turn off LP loop and enter hibernate).
+ *        - APPCTRL_SHUTDOWN: Stop the measurement immediately and put AFE to shut down mode(turn off LP loop and enter hibernate).
  * @return none.
 */
 AD5940Err AppSWVCtrl(uint32_t Command, void *pPara)
@@ -222,13 +242,13 @@ AD5940Err AppSWVCtrl(uint32_t Command, void *pPara)
     {
       WUPTCfg_Type wupt_cfg;
 
-      if(AD5940_WakeUp(10) > 10)  /* Wakup AFE by read register, read 10 times at most */
+      if(AD5940_WakeUp(10) > 10)  /* Wakeup AFE by read register, read 10 times at most */
         return AD5940ERR_WAKEUP;  /* Wakeup Failed */
       if(AppSWVCfg.SWVInited == bFALSE)
         return AD5940ERR_APPERROR;
       /**
        *  SWV example is special, because the sequence is dynamically generated. 
-       *  Before 'START' ramp test, call AppSWVInit firslty. 
+       *  Before 'START' ramp test, call AppSWVInit firstly. 
        */
       if(AppSWVCfg.RampState == SWV_STOP)
         return AD5940ERR_APPERROR;
@@ -251,11 +271,13 @@ AD5940Err AppSWVCtrl(uint32_t Command, void *pPara)
     }
     case APPCTRL_STOPNOW:
     {
-      if(AD5940_WakeUp(10) > 10)  /* Wakup AFE by read register, read 10 times at most */
+      if(AD5940_WakeUp(10) > 10)  /* Wakeup AFE by read register, read 10 times at most */
         return AD5940ERR_WAKEUP;  /* Wakeup Failed */
       /* Start Wupt right now */
       AD5940_WUPTCtrl(bFALSE);
-      AD5940_WUPTCtrl(bFALSE);  /* @todo is it sure this will stop Wupt? */
+      /* There is chance this operation will fail because sequencer could put AFE back 
+        to hibernate mode just after waking up. Use STOPSYNC is better. */
+      AD5940_WUPTCtrl(bFALSE);
       break;
     }
     case APPCTRL_STOPSYNC:
@@ -265,7 +287,7 @@ AD5940Err AppSWVCtrl(uint32_t Command, void *pPara)
     }
     case APPCTRL_SHUTDOWN:
     {
-      AppSWVCtrl(APPCTRL_STOPNOW, 0);  /* Stop the measurment if it's running. */
+      AppSWVCtrl(APPCTRL_STOPNOW, 0);  /* Stop the measurement if it's running. */
       AD5940_ShutDownS();
     }
     break;
@@ -301,7 +323,7 @@ static AD5940Err AppSWVSeqInitGen(void)
   aferef_cfg.Hp1V8Ilimit = bFALSE;
   aferef_cfg.Lp1V1BuffEn = bFALSE;
   aferef_cfg.Lp1V8BuffEn = bFALSE;
-  /* LP reference control - turn off them to save powr*/
+  /* LP reference control - turn off them to save power*/
   aferef_cfg.LpBandgapEn = bTRUE;
   aferef_cfg.LpRefBufEn = bTRUE;
   aferef_cfg.LpRefBoostEn = bFALSE;
@@ -315,9 +337,9 @@ static AD5940Err AppSWVSeqInitGen(void)
   lploop_cfg.LpAmpCfg.LpTiaRload = LPTIARLOAD_10R;
   lploop_cfg.LpAmpCfg.LpTiaRtia = AppSWVCfg.LPTIARtiaSel;
   if(AppSWVCfg.LPTIARtiaSel == LPTIARTIA_OPEN) /* User want to use external RTIA */
-    lploop_cfg.LpAmpCfg.LpTiaSW = LPTIASW(13)|LPTIASW(2)|LPTIASW(4)|LPTIASW(5)|LPTIASW(9)/*|LPTIASW(10)*/; /* SW5/9 is closed to support external RTIA resistor */
+    lploop_cfg.LpAmpCfg.LpTiaSW = /*LPTIASW(13)|*/LPTIASW(2)|LPTIASW(4)|LPTIASW(5)|LPTIASW(9)/*|LPTIASW(10)*/; /* SW5/9 is closed to support external RTIA resistor */
   else
-    lploop_cfg.LpAmpCfg.LpTiaSW = /*LPTIASW(13)|*/LPTIASW(2)|LPTIASW(8);
+    lploop_cfg.LpAmpCfg.LpTiaSW = /*LPTIASW(13)|*/LPTIASW(2)|LPTIASW(4)|LPTIASW(5);     /* Configured for 3 lead sensor connected to CE0, RE0 and SE0. Modify these switches if using a 2 lead sensor */
   lploop_cfg.LpDacCfg.LpdacSel = LPDAC0;
   lploop_cfg.LpDacCfg.DacData12Bit = 0x800;
   lploop_cfg.LpDacCfg.DacData6Bit = 0;
@@ -325,8 +347,8 @@ static AD5940Err AppSWVSeqInitGen(void)
   lploop_cfg.LpDacCfg.LpDacSW = LPDACSW_VBIAS2LPPA/*|LPDACSW_VBIAS2PIN*/|LPDACSW_VZERO2LPTIA/*|LPDACSW_VZERO2PIN*/;
   lploop_cfg.LpDacCfg.LpDacRef = LPDACREF_2P5;
   lploop_cfg.LpDacCfg.LpDacSrc = LPDACSRC_MMR;
-  lploop_cfg.LpDacCfg.LpDacVbiasMux = LPDACVBIAS_12BIT; /* Step Vbias. Use 12bit DAC ouput */
-  lploop_cfg.LpDacCfg.LpDacVzeroMux = LPDACVZERO_6BIT;  /* Base is Vzero. Use 6 bit DAC ouput */
+  lploop_cfg.LpDacCfg.DacData6Bit = (uint32_t)((AppSWVCfg.VzeroStart-200)/DAC6BITVOLT_1LSB);
+  lploop_cfg.LpDacCfg.DacData12Bit = (int32_t)((AppSWVCfg.RampStartVolt)/DAC12BITVOLT_1LSB) + lploop_cfg.LpDacCfg.DacData6Bit*64;
   lploop_cfg.LpDacCfg.PowerEn = bTRUE;
   AD5940_LPLoopCfgS(&lploop_cfg);
   
@@ -341,13 +363,14 @@ static AD5940Err AppSWVSeqInitGen(void)
   dsp_cfg.ADCFilterCfg.Sinc2NotchEnable = bTRUE;
   dsp_cfg.ADCFilterCfg.BpNotch = bTRUE;
   dsp_cfg.ADCFilterCfg.ADCSinc2Osr = ADCSINC2OSR_1067;  /* Don't care */
-  dsp_cfg.ADCFilterCfg.ADCAvgNum = ADCAVGNUM_2;   /* Don't care becase it's disabled */
+  dsp_cfg.ADCFilterCfg.ADCAvgNum = ADCAVGNUM_2;   /* Don't care because it's disabled */
   AD5940_DSPCfgS(&dsp_cfg);
+
   /* Sequence end. */
-  AD5940_SEQGenInsert(SEQ_STOP()); /* Add one extral command to disable sequencer for initialization sequence because we only want it to run one time. */
+  AD5940_SEQGenInsert(SEQ_STOP()); /* Add one extra command to disable sequencer for initialization sequence because we only want it to run one time. */
 
   /* Stop sequence generator here */
-  AD5940_SEQGenCtrl(bFALSE); /* Stop seuqncer generator */
+  AD5940_SEQGenCtrl(bFALSE); /* Stop sequencer generator */
   error = AD5940_SEQGenFetchSeq(&pSeqCmd, &SeqLen);
   if(error == AD5940ERR_OK)
   {
@@ -400,7 +423,7 @@ static AD5940Err AppSWVSeqADCCtrlGen(void)
   AD5940_EnterSleepS();/* Goto hibernate */
   /* Sequence end. */
   error = AD5940_SEQGenFetchSeq(&pSeqCmd, &SeqLen);
-  AD5940_SEQGenCtrl(bFALSE); /* Stop seuqncer generator */
+  AD5940_SEQGenCtrl(bFALSE); /* Stop sequencer generator */
 
   if(error == AD5940ERR_OK)
   {
@@ -673,7 +696,7 @@ AD5940Err AppSWVInit(uint32_t *pBuffer, uint32_t BufferSize)
   FIFOCfg_Type fifo_cfg;
   SEQCfg_Type seq_cfg;
   
-  if(AD5940_WakeUp(10) > 10)  /* Wakup AFE by read register, read 10 times at most */
+  if(AD5940_WakeUp(10) > 10)  /* Wakeup AFE by read register, read 10 times at most */
     return AD5940ERR_WAKEUP;  /* Wakeup Failed */
 
   /* Configure sequencer and stop it */
@@ -783,9 +806,9 @@ static int32_t AppSWVDataProcess(int32_t * const pData, uint32_t *pDataCount)
   float temp;
   for(i=0;i<datacount;i++)
   {
-    pData[i] &= 0xffff; 
-    temp = AD5940_ADCCode2Volt(pData[i], AppSWVCfg.AdcPgaGain, 1.82);
-    pOut[i] = temp/AppSWVCfg.RtiaValue.Magnitude*1e6f;     /* Result unit is uA. */
+    pData[i] &= 0xffff;
+    temp = AD5940_ADCCode2Volt(pData[i],AppSWVCfg.AdcPgaGain, AppSWVCfg.ADCRefVolt);
+    pOut[i] = -temp/AppSWVCfg.RtiaValue.Magnitude*1e3f;  /* Result unit is uA. */
   }
   return 0;
 }
@@ -803,7 +826,7 @@ AD5940Err AppSWVISR(void *pBuff, uint32_t *pCount)
   BuffCount = *pCount;
   uint32_t IntFlag;
 
-  if(AD5940_WakeUp(10) > 10)  /* Wakup AFE by read register, read 10 times at most */
+  if(AD5940_WakeUp(10) > 10)  /* Wakeup AFE by read register, read 10 times at most */
     return AD5940ERR_WAKEUP;  /* Wakeup Failed */
   AD5940_SleepKeyCtrlS(SLPKEY_LOCK);
   *pCount = 0;
